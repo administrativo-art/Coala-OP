1c9fe14b1e6d626b0654e9551126c6fcb9998875
Coala Shakes <administrativo@coalashakes.com>
2025-06-26 17:59:58 +0000

    mas já estava assim

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCn2V94gjX_Y1n4IOY40Y0JBKgXl--Sgns",
   authDomain: "smart-converter-752gf.firebaseapp.com",
   projectId: "smart-converter-752gf",
-  storageBucket: "smart-converter-752gf.appspot.com",
+  storageBucket: "smart-converter-752gf.firebasestorage.app",
   messagingSenderId: "787876557774",
   appId: "1:787876557774:web:cf2b2c3d7d0aae313a319f"
 };
