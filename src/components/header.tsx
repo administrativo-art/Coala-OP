34e1d1d212c3ef5ec4e3d3146ae6a843ef72fbbc
Coala Shakes <administrativo@coalashakes.com>
2025-06-27 17:42:59 +0000

    Melhora a fonte e deixe em negrito:"Oi, humano. Acredite em você!"

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import Link from "next/link"
@@ -43,7 +44,7 @@ export function Header() {
                 <nav className="grid gap-2 text-lg font-medium">
                     <Link href="/dashboard" className="font-logo select-none mb-4">
                         <div className="text-left text-2xl font-bold text-primary leading-none">coala</div>
-                        <div className="text-left text-xl font-bold text-accent -mt-1.5 pl-2.5 leading-none">shakes</div>
+                        <div className="text-left text-xl font-bold text-accent -mt-[0.4em] pl-[0.5em]">shakes</div>
                     </Link>
                     {navItems.map(item => item.show && (
                     <SheetTrigger asChild key={item.href}>
@@ -60,7 +61,7 @@ export function Header() {
             </SheetContent>
         </Sheet>
         <div className="w-full flex-1">
-            <p className="hidden text-base text-muted-foreground md:block">
+            <p className="hidden font-logo text-lg font-bold text-foreground md:block">
               Oi, humano. Acredite em você!
             </p>
         </div>
