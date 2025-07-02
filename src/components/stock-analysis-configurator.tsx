dd923d95463cc8eb5637593deacd12dec5d9a71e
Coala Shakes <administrativo@coalashakes.com>
2025-07-02 11:37:44 +0000

    NÃO DÁ DE CORRIGIR?

--- src/components/stock-analysis-configurator.tsx
+++ src/components/stock-analysis-configurator.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 import React, { useEffect, useState, useRef } from 'react';
 import { useForm, useFieldArray } from 'react-hook-form';
@@ -371,7 +372,7 @@ export function StockAnalysisConfigurator({ onAddNew, onEdit, onDelete }: StockA
                     control={form.control}
                     name={`products.${index}.pdfUnit`}
                     render={({ field: pdfUnitField }) => {
-                      const categoryUnits = Object.keys(units[field.category]);
+                      const categoryUnits = field.category ? Object.keys(units[field.category]) : [];
                       return (
                         <FormItem className="pt-2">
                           <FormLabel>Unidade de Medida no Relatório (PDF)</FormLabel>
