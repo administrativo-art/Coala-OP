b582831ef447bfb14b49bfe9fdb6e61730dc1e24
Coala Shakes <administrativo@coalashakes.com>
2025-07-07 20:37:46 +0000

    após movimentar mais para o fim do mês, a coluna Quiosque some.

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -58,7 +58,7 @@ export default function DashboardLayout({
   return (
     <div className={cn("grid min-h-screen w-full", isCollapsed ? "md:grid-cols-[80px_1fr]" : "md:grid-cols-[280px_1fr]")}>
       <Sidebar isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />
-      <div className="flex flex-col overflow-hidden">
+      <div className="flex flex-col">
         <Header />
         <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-background overflow-y-auto">
           {children}
