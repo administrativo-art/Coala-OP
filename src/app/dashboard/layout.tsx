2ad9988b77c85ac9e51491558bb5c075027f4a5a
Coala Shakes <administrativo@coalashakes.com>
2025-07-07 20:45:11 +0000

    não prestou

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -58,9 +58,9 @@ export default function DashboardLayout({
   return (
     <div className={cn("grid min-h-screen w-full", isCollapsed ? "md:grid-cols-[80px_1fr]" : "md:grid-cols-[280px_1fr]")}>
       <Sidebar isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />
-      <div className="flex flex-col">
+      <div className="flex flex-col min-w-0">
         <Header />
-        <main className="flex-1 p-4 lg:p-6 bg-background">
+        <main className="p-4 lg:p-6 bg-background">
           {children}
         </main>
       </div>
