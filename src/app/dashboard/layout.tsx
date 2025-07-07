5b67d9c91efb9c878575589b5b8364514b8e75c8
Coala Shakes <administrativo@coalashakes.com>
2025-07-07 20:40:10 +0000

    A coluna quiosque ainda não está totalmente fixa.

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -59,7 +59,7 @@ export default function DashboardLayout({
       <Sidebar isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />
       <div className="flex flex-col min-w-0">
         <Header />
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-background overflow-y-auto">
+        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-background">
           {children}
         </main>
       </div>
