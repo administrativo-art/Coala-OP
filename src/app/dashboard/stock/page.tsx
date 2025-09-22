9bb85ba2319c13692bc686a8de124957a26135bf
Coala Shakes <administrativo@coalashakes.com>
2025-09-22 18:44:36 +0000

    MESMA COISA

--- src/app/dashboard/stock/page.tsx
+++ src/app/dashboard/stock/page.tsx
@@ -1,4 +1,7 @@
-import { StockManagement } from './management';
+
+"use client";
+
+import { StockManagement } from '@/components/stock-management';
 
 export default function StockPage() {
     return <StockManagement />
