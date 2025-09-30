54f7cff4c4ba4a7a403ea905f927838abc8e5e5a
Coala Shakes <administrativo@coalashakes.com>
2025-09-30 21:58:26 +0000

    Podemos remover esses dois antigos. Certo?

--- src/components/auth-layout.tsx
+++ src/components/auth-layout.tsx
@@ -1,19 +1,2 @@
-
-"use client";
-
-// This component is no longer necessary as the logic has been consolidated
-// into the main AuthProvider. It can be removed.
-import React, { useContext } from 'react';
-import { AuthProvider } from './auth-provider';
-import { ProfilesProvider } from './profiles-provider';
-
-
-export function AuthLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <ProfilesProvider>
-      <AuthProvider>
-        {children}
-      </AuthProvider>
-    </ProfilesProvider>
-  );
-}
+// This file is deprecated and no longer in use.
+// It can be safely removed from the project.
