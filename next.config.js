05b925f25519d4751b1898c9af70026632d996c1
Coala Shakes <administrativo@coalashakes.com>
2026-03-13 20:49:39 +0000

    Substitua o conteúdo do `next.config.js` por:
    ```js
    /** @type {import('

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@react-pdf/renderer'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.openfoodfacts.org',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
